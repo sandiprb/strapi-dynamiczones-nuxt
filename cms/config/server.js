8efe26b16a6878ba8e0a08704af8ff7e8eace06a
Sandip Baradiya <baradiya.sandip@gmail.com>
2020-12-10 12:05:58 +0530

    init

---
+++ cms/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'aea819cfde6cdd7459479b0e919bfbab'),
+    },
+  },
+});
---
+++ cms/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'b33c8aba-4e2d-476f-acc8-065bdafe7e22'
+};
